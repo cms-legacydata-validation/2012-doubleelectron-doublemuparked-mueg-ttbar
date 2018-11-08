2e329a4d84d223083f3e17d675f11a3748df5fd0
Oleksandr Zenaiev <oleksandr.zenaiev@cern.ch>
2018-11-08 16:17:29 +0100

    improved compilation script

--- PostAnalyzer/compile.sh
+++ PostAnalyzer/compile.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # compile code (produces two executables)
-g++ -o ttbarMakeHist `root-config --cflags --libs` -lMathMore -std=c++11 ttbarMakeHist.cxx
-g++ -o ttbarMakePlots `root-config --cflags --libs` -std=c++11 ttbarMakePlots.cxx
+g++ ttbarMakeHist.cxx -o ttbarMakeHist `root-config --cflags --libs` -lMathMore -std=c++11
+g++ ttbarMakePlots.cxx -o ttbarMakePlots `root-config --cflags --libs` -std=c++11
 
 # create needed directories if do not exist yet
 mkdir -p data mc hist plots
